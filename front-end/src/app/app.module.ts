1100561560b9f9467ba95c37a08ca4466fd636d7
kirtmartin71@gmail.com <kirtmartin71@gmail.com>
2020-10-22 14:35:21 -0400

    Initial Commit

---
+++ front-end/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PropertyCardComponent } from './components/property-card/property-card.component';
+import { PropertyListComponent } from './components/property-list/property-list.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PropertyCardComponent,
+    PropertyListComponent,
+    NavBarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ front-end/src/app/components/property-card/property-card.component.ts
@@ -0,0 +1,18 @@
+import { Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-property-card',
+  templateUrl: './property-card.component.html',
+  styleUrls: ['./property-card.component.css']
+})
+export class PropertyCardComponent implements OnInit {
+
+  @Input() property: any;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
