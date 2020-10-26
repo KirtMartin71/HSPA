2e4e4a51e56c9048da74f46c60c1582609cda490
kirtmartin71@gmail.com <kirtmartin71@gmail.com>
2020-10-26 14:22:03 -0400

    Reset Project

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,68 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {HttpClientModule} from '@angular/common/http';
-import {Routes, RouterModule} from '@angular/router';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
-import { TabsModule } from 'ngx-bootstrap/tabs';
-import { ButtonsModule } from 'ngx-bootstrap/buttons';
-import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PropertyCardComponent } from './property/property-card/property-card.component';
-import { PropertyListComponent } from './property/property-list/property-list.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { HousingService } from './services/housing.service';
-import { AddPropertyComponent } from './property/add-property/add-property.component';
-import { PropertyDetailComponent } from './property/property-detail/property-detail.component';
-import { UserRegisterComponent } from './user/user-register/user-register.component';
-import { UserLoginComponent } from './user/user-login/user-login.component';
-import { UserService } from './services/user.service';
-import { AlertifyService } from './services/alertify.service';
-import { AuthService } from './services/auth.service';
-
-const appRoutes: Routes = [
-  {path: '', component: PropertyListComponent},
-  {path: 'rent-property', component: PropertyListComponent},
-  {path: 'add-property', component: AddPropertyComponent},
-  {path: 'property-detail/:id', component: PropertyDetailComponent},
-  {path: 'user/login', component: UserLoginComponent},
-  {path: 'user/register', component: UserRegisterComponent},
-  {path: '**', component: PropertyListComponent}
-]
+import { PropertyCardComponent } from './components/property-card/property-card.component';
+import { PropertyListComponent } from './components/property-list/property-list.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @NgModule({
-   declarations: [
-      AppComponent,
-      PropertyCardComponent,
-      PropertyListComponent,
-      NavBarComponent,
-      AddPropertyComponent,
-      PropertyDetailComponent,
-      UserRegisterComponent,
-      UserLoginComponent
-   ],
-   imports: [
-      BrowserModule,
-      HttpClientModule,
-      FormsModule,
-      ReactiveFormsModule,
-      RouterModule.forRoot(appRoutes),
-      BrowserAnimationsModule,
-      BsDropdownModule.forRoot(),
-      TabsModule.forRoot(),
-      ButtonsModule.forRoot(),
-      BsDatepickerModule.forRoot()
-   ],
-   providers: [
-     HousingService,
-     UserService,
-     AlertifyService,
-     AuthService
-   ],
-   bootstrap: [
-      AppComponent
-   ]
+  declarations: [
+    AppComponent,
+    PropertyCardComponent,
+    PropertyListComponent,
+    NavBarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
